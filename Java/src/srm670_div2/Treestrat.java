045fa31e184a06125b037ea91ca74ff8ee88c9d3
John Soh <johnsoh.01@gmail.com>
2015-12-23 21:23:31 +0800

    proper graph init

--- Java/src/srm670_div2/Treestrat.java
+++ Java/src/srm670_div2/Treestrat.java
@@ -1,32 +1,31 @@
 package srm670_div2;
 
-import java.util.Arrays;
-
 public class Treestrat {
 
 	public static int roundcnt(int[] tree, int[] A, int[] B)
 	{
 		int totalNodes = tree.length + 1;
 		int[][] allShortestPaths = new int[totalNodes][totalNodes];
-		int INFINITE = Integer.MAX_VALUE;
+		int INFINITE = Integer.MAX_VALUE/4;
 		
 		// set all to max
 		for(int i = 0; i < totalNodes; i++) {
-			Arrays.fill(allShortestPaths[0], INFINITE);
-			allShortestPaths[i][i] = 0;
+			for(int j = 0; j < totalNodes; j++) {
+				allShortestPaths[i][j] = i==j ? 0 : INFINITE;
+			}
 		}
 		
 		// create links for neighboring nodes
-		for(int i = 1; i < totalNodes; i++) {
-			int thisPosition = i + 1;
+		for(int i = 0; i < totalNodes-1; i++) {
+			int thisPosition = i + 1; //node i's neighbour at tree[i-1]
 			int nextPosition = tree[i];
 			allShortestPaths[thisPosition][nextPosition] = 1;
 			allShortestPaths[nextPosition][thisPosition] = 1;
 		}
 		
 		// Floyd-Warshall Algorithm
-		for (int i = 0; i < totalNodes; i++) {
-			for (int j = 0; j < totalNodes; j++) {
+		for (int j = 0; j < totalNodes; j++) {
+			for (int i = 0; i < totalNodes; i++) {
 				for (int k = 0; k < totalNodes; k++) {
 					int ik = allShortestPaths[i][k];
 					int ijk = allShortestPaths[i][j] + allShortestPaths[j][k];
@@ -40,14 +39,13 @@ public class Treestrat {
 		for(int prey : A) {
 			for(int hunter : B) {
 				int dist = howFarCanPreyMakeHunterWalk(allShortestPaths, prey, hunter);
-				if (dist < min) min = dist;
+				min = Math.min(min, dist);
 			}
 		}
 		
 		return min;
 	}
 	
-	// returns the maximum distance prey can make hunter walk
 	private static int howFarCanPreyMakeHunterWalk(int[][] allShortestPaths, int prey, int hunter) {
 		
 		int totalNodes = allShortestPaths[0].length;
@@ -59,7 +57,7 @@ public class Treestrat {
 			int preyAdvantage = distFromHunter - distFromPrey;
 			
 			if (preyAdvantage < 0) continue; // because prey would not go there 
-			if (preyAdvantage > maxDistance) maxDistance = preyAdvantage; // if this the max prey can make hunter walk so far
+			maxDistance = Math.max(maxDistance, preyAdvantage);
 		}
 		
 		return maxDistance;
