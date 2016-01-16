2c3342ff1376f623256f1adb0210f1e0807f3a3a
John Soh <johnsoh.01@gmail.com>
2016-01-16 18:47:37 +0800

    cpp issues

--- Cpp/Source/Main.cpp
+++ Cpp/Source/Main.cpp
@@ -2,8 +2,16 @@
 
 using namespace std;
 
-static void main() {
-	cout << "hello";
+int main() {
+	cout << "hello";	
 	int k;
 	cin >> k;
 }
+
+/*
+New Issues
+-> Creating new project
+--> Need to create empty project THEN add existing files from source folder
+--> Create project from exisiting files DOES NOT work 
+-> TODO: figure maintain file structure in cpp projects 
+*/
