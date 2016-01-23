cfd860f91847cbfe4dcfffd0d986851accd923e7
John Soh <johnsoh.01@gmail.com>
2016-01-23 10:13:45 +0800

    corrected continous binary search

--- Cpp/Cpp/2012R1B.cpp
+++ Cpp/Cpp/2012R1B.cpp
@@ -7,6 +7,11 @@ using namespace std;
 
 void SafetyInNumbers::solve() {
 
+	// check current path 
+	/*char path[FILENAME_MAX];
+	_getcwd(path, sizeof(path));
+	cout << path << endl;*/
+
 	ifstream in("2012R1B.txt");
 	cin.rdbuf(in.rdbuf());
 	int rounds, players, score;
@@ -35,26 +40,32 @@ void SafetyInNumbers::find(vector<int> v) {
 		// do b search
 		double high = 1; double low = 0; double curr = 0.5; double prev = 2;
 		bool safe = false;
-		while (!safe || abs(curr - prev) > 0.0001) {
+		// order of conditions is important as first condition only gets smaller and 
+		// second alternates between true and false fairly often. 
+		while (abs(curr - prev) > 0.0000001 || !safe) {
 			// is curr safe?
-			double audience = curr;
+			double audience = curr; 
 			double currScore = v[i] + audience*audienceWeight;
+			audience = 1 - audience; // the audience left to distribute to the others
 			For(j, indexes) {
 				if (i == j) continue;
+				// if curr contestant has less than the other, it is sure loose. 
 				if (currScore < v[j]) {
 					safe = false;
 					break;
 				}
 				double diff = currScore - v[j];
 				audience -= diff / audienceWeight;
+				safe = audience <= 0; // we are safe if there is no more audience to distribute to our adversaries
 			}
-			safe = audience <= 0;
+			
+			//if (safe && audience > 0) safe = false;
 
 			// adjust based on safety
 			if (safe) high = curr;
 			else low = curr;
 			prev = curr;
-			curr = high + low / 2;
+			curr = (high + low) / 2;
 		}
 		cout << curr << " ";
 	}
--- Cpp/Cpp/Main.cpp
+++ Cpp/Cpp/Main.cpp
@@ -8,30 +8,8 @@
 using namespace std;
 
 int main() {
-	cout << "hello" <<endl ;
-
-	// check current path 
-	char path[FILENAME_MAX];
-	_getcwd(path, sizeof(path));
-	cout << path << endl;
-
-	// redirect cin 
-	ifstream in("2012R1B.txt");
-	cin.rdbuf(in.rdbuf());
-	int round;
-	cin >> round;
-	cout << endl << round << endl;
-
 	SafetyInNumbers s;
 	s.solve();
 
 	getchar(); // pause 
 }
-
-/*
-New Issues
--> Creating new project
---> Need to create empty project THEN add existing files from source folder
---> Create project from exisiting files DOES NOT work 
--> TODO: figure maintain file structure in cpp projects 
-*/
