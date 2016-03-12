fbd977ca74d6875067f33d80eb03f5b4e64936f2
johnsoh <johnsoh.01@gmail.com>
2016-03-12 17:29:41 +0800

    cpp added

--- Cpp/Cpp/Main.cpp
+++ Cpp/Cpp/Main.cpp
@@ -4,12 +4,14 @@
 #include <stdio.h>
 #include <direct.h> // http://stackoverflow.com/questions/143174/how-do-i-get-the-directory-that-a-program-is-running-from
 #include "2012R1B.h"
+#include "Istr.h"
 
 using namespace std;
 
 int main() {
-	SafetyInNumbers s;
-	s.solve();
-
+	//SafetyInNumbers s;
+	//s.solve();
+	Istr istr;
+	istr.solve2();
 	getchar(); // pause 
 }
